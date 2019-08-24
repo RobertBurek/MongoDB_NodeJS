cfd8ca1b2f6fb49918a15eb5938bc0c20a32bdce
Robert Burek <robertburek@wp.pl>
2019-08-24 22:39:54 +0200

    Dodanie dokumentu do bazy

---
+++ appInsert.js
@@ -0,0 +1,32 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/';
+
+MongoClient.connect(url, function(err, client){
+    if (err) {
+        console.log(err);
+    } else {
+        var db = client.db('db_kurs_mongo');
+        console.log("Połączono z bazą db_kurs_mongo");
+        insertDocument(db, function(){
+           client.close();
+        });
+    }
+});
+
+var insertDocument = function(db, callback) {
+    db.collection('products').insertOne(
+        {
+            name: "X-New Product",
+            price: 250,
+            category: "software",
+            stock: 35
+        },function(err, result){
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Wstawiono nowy dokument do bazy." + result);
+                callback();
+            }
+    });
+};
