19259b22c384e87a4dc22858ad054529981bfec1
Robert Burek <robertburek@wp.pl>
2019-08-24 22:24:48 +0200

    Odczyt zawartości kolekcji

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/db_kurs_mongo';
+var url = 'mongodb://localhost:27017/';
 
-MongoClient.connect(url, function(err, db){
+MongoClient.connect(url, function(err, client){
     if (err) {
         console.log(err);
     } else {
+        var db = client.db('db_kurs_mongo');
         console.log("Połączono z bazą db_kurs_mongo");
+        listProducts(db, function(){
+           client.close();
+        });
     }
-    db.close();
 });
+
+var listProducts = function(db, callback) {
+    var cursor = db.collection('products').find(
+        { category: "software" }, 
+        // { _id: 0, name: 1, price: 1 }
+    );
+    cursor.each(function(err, doc){
+        console.log(doc);
+        callback();
+    });
+};
